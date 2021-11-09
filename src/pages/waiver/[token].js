3d8052d6a4db8b3c7a3bf68d2b1a49ec65f2755f
Lola <lola@egherman.com>
2021-11-08 19:02:28 -0600

    implement waivers

---
+++ src/pages/waiver/[token].js
@@ -0,0 +1,95 @@
+import React from 'react';
+import Text, { Heading, Link } from '@codeday/topo/Atom/Text';
+import Box from '@codeday/topo/Atom/Box'
+import Content from '@codeday/topo/Molecule/Content';
+import { sign, verify } from 'jsonwebtoken';
+import getConfig from 'next/config';
+import Page from '../../components/Page';
+import { GetTicketDetailsQuery } from './token.gql';
+import { apiFetch } from '@codeday/topo/utils';
+import { print } from 'graphql';
+import ErrorPage from '../../components/ErrorPage';
+import CognitoForm from '@codeday/topo/Molecule/CognitoForm'
+const { serverRuntimeConfig } = getConfig();
+export default function WaiverPage({token, ticket, error}) {
+  if (error) return <ErrorPage details={error} />
+  return (
+    <Page>
+      <Content>
+        <Box borderWidth={1} shadow="md" rounded="md" overflow="hidden">
+          <Box
+            bg="brand.700"
+            color="brand.50"
+            fontSize="xl"
+            p={4}
+            fontWeight="bold"
+            rounded="md"
+            borderBottomLeftRadius={0}
+            borderBottomRightRadius={0}
+          >
+            CodeDay Participation Waiver
+          </Box>
+          <Box p={8}>
+            <CognitoForm
+              formId={5}
+              prefill={{
+                JWT: token,
+                Minor: (ticket.age >= ticket.event.majorityAge? 'no': 'yes'),
+                Ticket: ticket.id,
+                ParticipantName: `${ticket.firstName} ${ticket.lastName}`,
+                EventName: ticket.event.name,
+                SignerEmail: ticket.guardian?.email
+              }}/>
+          </Box>
+        </Box>
+
+      </Content>
+    </Page>
+  );
+}
+
+export async function getServerSideProps({ params: { token } }) {
+  let jwt;
+  try {
+    jwt = verify(token, serverRuntimeConfig.clear_gql.secret, { audience: serverRuntimeConfig.audience });
+    if (!jwt) {
+      return {
+        props: {
+          error: 'JWT verification failed',
+        },
+      };
+    }
+  } catch (e) {
+    return {
+      props: {
+        error: e.toString(),
+      },
+    };
+  }
+  const clearToken = sign(
+    { t: 'A' },
+    serverRuntimeConfig.clear_gql.secret,
+    { audience: serverRuntimeConfig.clear_gql.audience, expiresIn: '10s' },
+  );
+
+  const { clear } = await apiFetch(
+    print(GetTicketDetailsQuery),
+    { ticketId: jwt.tid },
+    { 'X-Clear-Authorization': `Bearer ${clearToken}` },
+  );
+  const { ticket } = clear;
+  if (!ticket) {
+    return {
+      props: {
+        error: 'Ticket not found',
+      },
+    };
+  }
+
+  return {
+    props: {
+      token,
+      ticket,
+    },
+  };
+}
