74841d27f1c7276eb4ba6f8dee85667ddfbc2b86
Tyler Menezes <tylermenezes@codeday.org>
2022-05-23 12:30:18 -0400

    Fix lint

--- src/components/PaymentBox.js
+++ src/components/PaymentBox.js
@@ -4,10 +4,12 @@ import { PaymentProvider } from './PaymentProvider';
 export default function PaymentBox({
   event, ...props
 }) {
-  return <PaymentProvider
-    paymentProvider={event?.region?.paymentProvider || 'stripe'}
-    currency={event?.region?.currency || 'usd'}
-    event={event}
-    {...props}
-  />;
+  return (
+    <PaymentProvider
+      paymentProvider={event?.region?.paymentProvider || 'stripe'}
+      currency={event?.region?.currency || 'usd'}
+      event={event}
+      {...props}
+    />
+  );
 }
