686009c6eac572448368d55cbaa4c9f28425479f
Tyler Menezes <tylermenezes@codeday.org>
2022-06-01 11:46:34 -0400

    Add whatsapp support for enabled regions

--- src/components/PhoneInput.js
+++ src/components/PhoneInput.js
@@ -1,22 +1,43 @@
 import { useState } from 'react';
-import { TextInput } from '@codeday/topo/Atom';
+import { TextInput, RadioGroup, Radio, Box, Text } from '@codeday/topo/Atom';
 import { normalizePhone } from '../../utils';
 
 export default function PhoneBox({ onChange, region, ...props }) {
   const [isValid, setIsValid] = useState(false);
   const [entered, setEntered] = useState('');
+  const [normalized, setNormalized] = useState('');
+  const [isWhatsApp, setIsWhatsApp] = useState(false);
 
   return (
-    <TextInput
-      value={entered || ''}
-      borderColor={(entered && !isValid) ? 'red.800' : undefined}
-      onChange={(e) => {
-        const { phoneNumber, isValid: _isValid } = normalizePhone(e.target.value, region);
-        setEntered(e.target.value);
-        setIsValid(_isValid);
-        onChange(phoneNumber, !e.target.value || _isValid);
-      }}
-      {...props}
-    />
+    <Box>
+      <TextInput
+        value={entered || ''}
+        borderColor={(entered && !isValid) ? 'red.800' : undefined}
+        onChange={(e) => {
+          const { phoneNumber, isValid: _isValid } = normalizePhone(e.target.value, region);
+          setEntered(e.target.value);
+          setNormalized(phoneNumber);
+          setIsValid(_isValid);
+          onChange(phoneNumber, !e.target.value || _isValid, isWhatsApp);
+        }}
+        {...props}
+      />
+      {entered && (
+        <Text fontSize="xs" color={!isValid ? 'red.700' : undefined}>
+          {!isValid
+            ? `Not a valid ${region.countryNameShortAdjective || 'US'} mobile phone number.`
+            : (entered && `Your full international number: ${normalized}`)}
+        </Text>
+      )}
+        {(region.messagingServices || ['sms']).includes('whatsApp') && (
+          <RadioGroup defaultValue="phone" mt={1} onChange={e => {
+            setIsWhatsApp(e === 'whatsApp');
+            onChange(normalized, isValid, e === "whatsApp");
+          }}>
+            <Radio value="phone" mr={4}>SMS</Radio>
+            <Radio value="whatsApp">WhatsApp</Radio>
+          </RadioGroup>
+        )}
+      </Box>
   );
 }
