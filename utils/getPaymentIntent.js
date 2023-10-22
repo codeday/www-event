8646c4235570b3a9ae081986e248d5fa19d87b57
Lola <lola@egherman.com>
2023-10-21 21:00:08 -0500

    catch other payment intent id case

--- utils/getPaymentIntent.js
+++ utils/getPaymentIntent.js
@@ -1,4 +1,7 @@
 export function getPaymentIntent(o) {
-    if (typeof o === 'object' && 'paymentIntentId' in o) return o.paymentIntentId;
-    return o;
+  if (typeof o === 'object') {
+    if ('paymentIntentId' in o) return o.paymentIntentId;
+    if ('paymentIntent' in o) return o.paymentIntent;
+  }
+  return o;
 }
