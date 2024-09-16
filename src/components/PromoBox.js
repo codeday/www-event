8d23316c72e1d2a074b32e78dab445f7b0c9503c
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2024-09-16 15:25:09 +0530

    Auto apply promo code from URL

--- src/components/PromoBox.js
+++ src/components/PromoBox.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import {
   Flex, Box, Button, TextInput, Text,
 } from '@codeday/topo/Atom';
@@ -7,6 +7,7 @@ import { UiCheck } from '@codeday/topocons/Icon';
 import { print } from 'graphql';
 import { useTranslation } from 'next-i18next';
 import { CheckPromoCode } from './PromoBox.gql';
+import { useRouter } from 'next/router'
 
 export default function PromoBox({ event, onChange, ...rest }) {
   const [promoCode, setPromoCode] = useState();
@@ -14,9 +15,48 @@ export default function PromoBox({ event, onChange, ...rest }) {
   const [show, setShow] = useState(event.requiresPromoCode);
   const { error, success } = useToasts();
   const { t } = useTranslation('Register');
+  const router = useRouter();
+  const autoAppliedPromo = useRef(false);
 
   const promoAppliedString = event.requiresPromoCode ? t('promo-applied.access-code', { promoCode }) : t('promo-applied.promo-code', { promoCode });
   const promoAskString = event.requiresPromoCode ? t('promo-ask.access-code') : t('promo-ask.promo-code');
+
+  const handleSubmitPromo = async () => {
+    if (!promoCode) {
+      onChange(undefined, undefined, undefined, undefined);
+      setShow(false);
+      return;
+    }
+
+    setIsLoading(true);
+    try {
+      const result = await apiFetch(print(CheckPromoCode), { id: event.id, code: promoCode });
+      const promoDetails = result?.clear?.findFirstEvent?.checkPromoCode;
+      if (!promoDetails?.valid) throw new Error(t('common:error.message.no-promo'));
+      if (promoDetails.remainingUses !== null && promoDetails.remainingUses <= 0) throw new Error(t('common:error.message.promo-fully-used'));
+      success(t('common:success.message.promo-applied', {
+        context: promoDetails.type,
+        promoName: promoDetails.displayDiscountName,
+        discountAmount: promoDetails.discountAmount,
+        currency: event.region.currency || 'USD',
+      }));
+      setShow(false);
+      onChange(promoCode, promoDetails.effectivePrice, promoDetails.remainingUses, promoDetails.metadata);
+    } catch (ex) {
+      error(ex.toString());
+      setPromoCode('');
+    }
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if (router.query.promo && typeof router.query.promo === 'string' && !!router.query.promo.trim() && !autoAppliedPromo.current) {
+      setShow(true);
+      setPromoCode(router.query.promo);
+      handleSubmitPromo()
+      autoAppliedPromo.current = true;
+    }
+  }, [router.query])
 
   if (!show) {
     return (
@@ -54,33 +94,7 @@ export default function PromoBox({ event, onChange, ...rest }) {
           colorScheme="green"
           isLoading={isLoading}
           disabled={event.requiresPromoCode && !promoCode}
-          onClick={async () => {
-            if (!promoCode) {
-              onChange(undefined, undefined, undefined, undefined);
-              setShow(false);
-              return;
-            }
-
-            setIsLoading(true);
-            try {
-              const result = await apiFetch(print(CheckPromoCode), { id: event.id, code: promoCode });
-              const promoDetails = result?.clear?.findFirstEvent?.checkPromoCode;
-              if (!promoDetails?.valid) throw new Error(t('common:error.message.no-promo'));
-              if (promoDetails.remainingUses !== null && promoDetails.remainingUses <= 0) throw new Error(t('common:error.message.promo-fully-used'));
-              success(t('common:success.message.promo-applied', {
-                context: promoDetails.type,
-                promoName: promoDetails.displayDiscountName,
-                discountAmount: promoDetails.discountAmount,
-                currency: event.region.currency || 'USD',
-              }));
-              setShow(false);
-              onChange(promoCode, promoDetails.effectivePrice, promoDetails.remainingUses, promoDetails.metadata);
-            } catch (ex) {
-              error(ex.toString());
-              setPromoCode('');
-            }
-            setIsLoading(false);
-          }}
+          onClick={handleSubmitPromo}
         >
           <UiCheck />
         </Button>
